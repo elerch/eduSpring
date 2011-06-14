68acbd03aff6f4e64cf9d9647faf421d328bcb71
Emil Lerch <emil@lerch.org>
2011-06-13 21:46:27 -0700

    Updating the fallacies of interfaces project for more fallacies

--- 2 - FallaciesOfInterfaces/FallaciesOfInterfaces.cs
+++ 2 - FallaciesOfInterfaces/FallaciesOfInterfaces.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -10,12 +11,52 @@ namespace FallaciesOfInterfaces
     {
         static void Main(string[] args)
         {
+            var myStringBuilder = new StringBuilder("foo");
+            var myString = myStringBuilder.ToString();
+            int result;
+
+            // I can assume myGuid.Value.ToString() == GuidText
+            // After all, I never modify the value...
+
+            // In dev, this works great!  It returns "1", which is parsed
             var worker = new UseThisInternallyForDev() as IDoSomething;
             Console.WriteLine(int.Parse(worker.DoSomething(null)));
+            Debug.Assert(myStringBuilder.ToString() == myString);
+
+            // Now in test, null doesn't work anymore. The implementation
+            // returns null, which will no longer parse to an int
+            // Interfaces don't protect us from this oversight
             worker = new UseThisInTest();
             Console.WriteLine(int.Parse(worker.DoSomething(null)));
+            // It fails, so we put in a null coalesce to cover the condition: 
+            //Console.WriteLine(int.Parse(worker.DoSomething(null) ?? "2" )); 
+            Debug.Assert(myStringBuilder.ToString() == myString);
+
             worker = new UseThisInProduction();
-            Console.WriteLine(int.Parse(worker.DoSomething(null)));
+            // Now we've got some solid code, but we get to production, and it 
+            // still fails!  For this implementation we can't pass in null,
+            // and the return value we get can't be parsed to an int either..
+            Console.WriteLine(int.Parse(worker.DoSomething(null) ?? "3"));
+            // So we modify to this:            
+            //if (int.TryParse(worker.DoSomething(myStringBuilder), out result))
+            //    Console.WriteLine(result);
+            //else
+            //    Console.WriteLine("3");
+            Debug.Assert(myStringBuilder.ToString() == myString);
+
+
+            // Now we've handled everything that could possibly go wrong,
+            // so we roll this out to a new environment and...
+            worker = new UseThisOnASeparateInstall();
+            if (int.TryParse(worker.DoSomething(myStringBuilder), out result))
+                Console.WriteLine(result);
+            else
+                Console.WriteLine("4");
+
+            // Assertion failure.  In this environment, our dependency has mucked 
+            // with our object on us!
+            Debug.Assert(myStringBuilder.ToString() == myString);
+
         }
     }
 
@@ -29,7 +70,7 @@ namespace FallaciesOfInterfaces
     {
         public string DoSomething(object foo)
         {
-            return "3";
+            return "1";
         }
     }
     class UseThisInTest : IDoSomething
@@ -49,7 +90,19 @@ namespace FallaciesOfInterfaces
             return foo.GetType().FullName;
         }
     }
-
+    class UseThisOnASeparateInstall : IDoSomething
+    {
+        public string DoSomething(object foo)
+        {
+            var myStringBuilder = foo as StringBuilder;
+            if (myStringBuilder != null) {
+                myStringBuilder.Clear();
+                myStringBuilder.Append("bar");
+            }
+            
+            return foo.ToString();
+        }
+    }
 
 
 }
